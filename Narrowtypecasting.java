c01049a8afa2bb68a908f813a15d8e0d328e9fac
SAURABH GUPTA <42804047+saurabhprasadsah@users.noreply.github.com>
2019-08-03 13:55:32 +0530

    narrowtypecasting

---
+++ Narrowtypecasting.java
@@ -0,0 +1,19 @@
+public class Typecasting {
+    public static void main(String args[]) {
+
+   //  int x = 4;
+     
+    // double y = x;
+
+     // System.out.print(x);
+      //  System.out.print(y);
+
+
+
+
+        double mydouble =2.23232309;
+        int myint = (int)mydouble;
+        System.out.println(mydouble);
+        System.out.println(myint);
+    }
+}
